0402e9e6b96b4d1b900a2926c106dc7bf9d5672e
Shreyash Saha <workshreyashsaha@gmail.com>
2026-07-28 17:08:57 +0530

    Time: 8 ms (16.13%), Space: 12.6 MB (50.87%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -7,8 +7,7 @@ class Solution(object):
         """
         n3 = nums1 + nums2
         n3.sort()
-        mid = len(n3)//2
         if len(n3) % 2 == 0:
-            return  (n3[mid] + n3[ mid - 1]) / 2.0
+            return  (n3[len(n3)//2] + n3[len(n3)//2 - 1]) / 2.0
         else :
-            return float(n3[mid])
+            return float(n3[len(n3)//2])
